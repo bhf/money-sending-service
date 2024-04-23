020d655e7ac71fd1c8c0f6ebfbecc8363b04cebb
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-04-23 20:27:38 +0100

    NEV-3 Structure for validating transfers between users

---
+++ MoneySendingService/src/main/java/com/neverless/sendingservice/entities/TransferRequest.java
@@ -0,0 +1,5 @@
+package com.neverless.sendingservice.entities;
+
+public class TransferRequest {
+
+}
---
+++ MoneySendingService/src/main/java/com/neverless/sendingservice/transfer/validation/TransferValidator.java
@@ -0,0 +1,34 @@
+package com.neverless.sendingservice.transfer.validation;
+
+import com.neverless.sendingservice.entities.TransferRequest;
+
+public interface TransferValidator {
+	
+	/**
+	 * Whether the transfer is valid
+	 * @return
+	 */
+	public TransferValidationResults transferIsValid(TransferRequest req);
+
+	/**
+	 * Check the source account exists and is valid
+	 * @return
+	 */
+	public boolean sourceAccountIsValid(TransferRequest req);
+	
+	/**
+	 * Check the destination account is valid and exists
+	 * @return
+	 */
+	public boolean destinationAccountIsValid(TransferRequest req);
+	
+	/**
+	 * Check the qty being transferred between users is valid
+	 * given any pending transfers and any pending withdrawals
+	 * @return
+	 */
+	public boolean qtyIsValid(TransferRequest req);
+	
+	
+	
+}
