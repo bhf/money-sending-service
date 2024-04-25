cda54dbd9e5a1dbca47fc3610c91d2f6a7c4bf5c
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-04-25 15:52:16 +0100

    NEV-4 Endpoints for transferMoney

--- REST/src/main/java/com/neverless/sendingservice/REST/dto/response/TransferReqResponseDTO.java
+++ REST/src/main/java/com/neverless/sendingservice/REST/dto/response/TransferReqResponseDTO.java
@@ -2,4 +2,8 @@ package com.neverless.sendingservice.REST.dto.response;
 
 public class TransferReqResponseDTO {
 
+	public boolean sourceAccountValid;
+	public boolean destinationAccountValid;
+	public boolean qtyIsValid;
+
 }
