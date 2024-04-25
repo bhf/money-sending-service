e438ef9b36763d490ae225612e7f86a63cd0fa37
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-04-25 13:16:01 +0100

    NEV-19 Source account validity

--- MoneySendingService/src/main/java/com/neverless/sendingservice/transfer/validation/TransferValidatorImpl.java
+++ MoneySendingService/src/main/java/com/neverless/sendingservice/transfer/validation/TransferValidatorImpl.java
@@ -1,8 +1,16 @@
 package com.neverless.sendingservice.transfer.validation;
 
+import com.neverless.sendingservice.entities.UserService;
 import com.neverless.sendingservice.entities.transactions.TransferRequest;
 
 public class TransferValidatorImpl implements TransferValidator{
+
+	final UserService userService;
+	
+	public TransferValidatorImpl(UserService userService) {
+		super();
+		this.userService = userService;
+	}
 
 	@Override
 	public TransferValidationResults transferIsValid(TransferRequest req) {
@@ -16,15 +24,22 @@ public class TransferValidatorImpl implements TransferValidator{
 
 	@Override
 	public boolean sourceAccountIsValid(TransferRequest req) {
-		
-		return false;
+		long userID = req.userId;
+		long assetId = req.assetId;
+
+		// check the user-asset combo exist
+
+		boolean isValid = userService.isValid(userID, assetId);
+		return isValid;
 	}
 
 	@Override
 	public boolean destinationAccountIsValid(TransferRequest req) {
-		
 		return false;
+		
 	}
+	
+	
 
 	@Override
 	public boolean qtyIsValid(TransferRequest req) {
