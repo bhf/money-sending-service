3e15f3844595f75eeca46917a64e658712af250e
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-04-25 11:11:04 +0100

    NEV-27 Test for adding a transfer

--- MoneySendingService/src/test/java/com/neverless/sendingservice/transactiontracker/TransactionTrackerImplTest.java
+++ MoneySendingService/src/test/java/com/neverless/sendingservice/transactiontracker/TransactionTrackerImplTest.java
@@ -1,30 +1,62 @@
 package com.neverless.sendingservice.transactiontracker;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.neverless.sendingservice.entities.AssetQty;
+import com.neverless.sendingservice.entities.transactions.TransferRequest;
+import com.neverless.sendingservice.transfer.TransferService;
+import com.neverless.sendingservice.withdraw.WithdrawalService;
+
+@ExtendWith(MockitoExtension.class)
 class TransactionTrackerImplTest {
 
 
 	@Test
 	void testGetPendingTransfers() {
-		fail("Not yet implemented");
+		
+		
+		
 	}
 
 	@Test
 	void testGetPendingWithdraws() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	void testAddPendingWithdraw() {
-		fail("Not yet implemented");
+	
 	}
 
 	@Test
 	void testAddPendingTransfer() {
-		fail("Not yet implemented");
+		WithdrawalService withdrawService=Mockito.mock(WithdrawalService.class);
+		TransferService transferService=Mockito.mock(TransferService.class);
+		
+		TransactionTracker t=new TransactionTrackerImpl(withdrawService, transferService);
+		
+		UUID requestId=UUID.randomUUID();
+		long assetId=1;
+		AssetQty qty=new AssetQty();
+		qty.assetId=assetId;
+		qty.qty=100;
+		long userId=1;
+		
+		TransferRequest pendingReq=new TransferRequest(requestId, qty, assetId);
+		t.addPendingTransfer(pendingReq, userId);
+		
+		PendingTransferTransactionSummary pendingTransfers = t.getPendingTransfers(userId);
+		
+		assertNotNull(pendingTransfers);
+		assertEquals(1, pendingTransfers.pendingTransfers.size());
 	}
 
 }
