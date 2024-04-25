293f4f89b73042300ab38561a8e2933d263b8417
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-04-25 11:42:46 +0100

    NEV-14 Validate existence of user-asset combo

--- MoneySendingService/src/main/java/com/neverless/sendingservice/withdraw/validation/WithdrawalValidatorImpl.java
+++ MoneySendingService/src/main/java/com/neverless/sendingservice/withdraw/validation/WithdrawalValidatorImpl.java
@@ -1,34 +1,47 @@
 package com.neverless.sendingservice.withdraw.validation;
 
+import com.neverless.sendingservice.entities.UserService;
 import com.neverless.sendingservice.entities.transactions.WithdrawRequest;
 
-public class WithdrawalValidatorImpl implements WithdrawalValidator{
+public class WithdrawalValidatorImpl implements WithdrawalValidator {
+
+	final UserService userService;
+
+	public WithdrawalValidatorImpl(UserService userService) {
+		super();
+		this.userService = userService;
+	}
 
 	@Override
 	public WithdrawalValidationResults validateWithdraw(WithdrawRequest req) {
-		WithdrawalValidationResults res=new WithdrawalValidationResults();
-		res.srcAccountValid=sourceAccountExists(req);
-		res.destIsValid=destinationAddressIsValid(req);
-		res.unequalSrcDest=unequalSourceDestination(req);
-		res.sufficientBalance=sourceBalanceIsSufficient(req);
+		WithdrawalValidationResults res = new WithdrawalValidationResults();
+		res.srcAccountValid = sourceAccountExists(req);
+		res.destIsValid = destinationAddressIsValid(req);
+		res.unequalSrcDest = unequalSourceDestination(req);
+		res.sufficientBalance = sourceBalanceIsSufficient(req);
 		return res;
 	}
 
 	@Override
 	public boolean sourceAccountExists(WithdrawRequest req) {
-		
-		return false;
+		long userID = req.userId;
+		long assetId = req.assetId;
+
+		// check the user-asset combo exist
+
+		boolean isValid=userService.isValid(userID,assetId);
+		return isValid;
 	}
 
 	@Override
 	public boolean destinationAddressIsValid(WithdrawRequest req) {
-		
+
 		return false;
 	}
 
 	@Override
 	public boolean sourceBalanceIsSufficient(WithdrawRequest req) {
-		
+
 		return false;
 	}
 
